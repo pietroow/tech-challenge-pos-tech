2bd19a08c74f18971dd5ac68e2946ea54df47420
Geovane Silva <geovanesilva.nar@hotmail.com>
2023-10-29 13:19:28 -0300

    Feat: Insert valus into Categoria and Status_Pedido.

--- src/main/resources/db/migration/V1__create_categoria.sql
+++ src/main/resources/db/migration/V1__create_categoria.sql
@@ -2,3 +2,8 @@ CREATE TABLE IF NOT EXISTS categoria (
                                          id INTEGER PRIMARY KEY,
                                          nome VARCHAR(100) NOT NULL
 );
+
+INSERT INTO categoria (id, nome) VALUES (1, 'Lanche');
+INSERT INTO categoria (id, nome) VALUES (2, 'Acompanhamento');
+INSERT INTO categoria (id, nome) VALUES (3, 'Bebida');
+INSERT INTO categoria (id, nome) VALUES (4, 'Sobremesa');
--- src/main/resources/db/migration/V7__create_status_pedido.sql
+++ src/main/resources/db/migration/V7__create_status_pedido.sql
@@ -2,3 +2,10 @@ CREATE TABLE IF NOT EXISTS status_pedido (
                                          id INTEGER PRIMARY KEY,
                                          nome VARCHAR(100) NOT NULL
 );
+
+INSERT INTO status_pedido (id, nome) VALUES (1, 'Realizado');
+INSERT INTO status_pedido (id, nome) VALUES (2, 'Pendente');
+INSERT INTO status_pedido (id, nome) VALUES (3, 'Confirmado');
+INSERT INTO status_pedido (id, nome) VALUES (4, 'Em preparação');
+INSERT INTO status_pedido (id, nome) VALUES (5, 'Concluído');
+INSERT INTO status_pedido (id, nome) VALUES (6, 'Cancelado');
